44e771d8962c660289bfff5de34275bdbd9e5f2c
lelonek1 <lelonek1@umbc.edu>
2024-04-16 10:50:34 -0400

    Update views.py with sqli

--- graphql_jwt/views.py
+++ graphql_jwt/views.py
@@ -2,6 +2,7 @@ import pickle
 from django.http import HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from django.db import connection
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -16,3 +17,16 @@ class MyVulnerableView(View):
         user_input = request.GET.get('user_input', '')
         return HttpResponse(f"Your input was: {user_input}")
 
+    def sqli_vulnerable_view(request):
+        name = request.GET.get('name', '')
+        #return HttpResponse(f"Your input was: {user_input}")
+    
+        if name:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
+                )
+                return HttpResponse(f"result: {cursor.fetchone()}")
+        return HttpResponse(f"no name provided")
+
+    
